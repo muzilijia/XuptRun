5d42b9398c56383b61818a371f0d101914d75bbe
muzijia <934762349@qq.com>
2018-05-05 19:48:44 +0800

    mine ok

--- app/src/main/java/com/muzi/xuptrun/view/fragment/ThirdFragment/ThirdFragment.java
+++ app/src/main/java/com/muzi/xuptrun/view/fragment/ThirdFragment/ThirdFragment.java
@@ -1,24 +1,28 @@
 package com.muzi.xuptrun.view.fragment.ThirdFragment;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.FindCallback;
+import com.avos.avoscloud.SaveCallback;
 import com.muzi.xuptrun.R;
 import com.muzi.xuptrun.view.activity.LoginActivity;
 
 import java.util.List;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by muzi on 2018/4/18.
@@ -26,8 +30,9 @@ import java.util.List;
 
 public class ThirdFragment extends Fragment implements View.OnClickListener {
     private EditText t_username,t_email,t_height,t_weight,t_target;
-    private String username,email,height,weight,target,userdataId;
+    private String username,email,height,weight,target;
     private Button edit,exit;
+    private SharedPreferences sPref;
 
     public ThirdFragment() {}
 
@@ -55,15 +60,21 @@ public class ThirdFragment extends Fragment implements View.OnClickListener {
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        //userId持久化，昵称邮箱不可更改
-                        AVObject userdata = AVObject.createWithoutData("UserData", userdataId);
+                        AVObject userdata = AVObject.createWithoutData("UserData", sPref.getString("userdataId", ""));
                         userdata.put("height", height);
                         userdata.put("weight", weight);
                         userdata.put("target", target);
-                        userdata.saveInBackground();
-                        t_height.setText(height);
-                        t_weight.setText(weight);
-                        t_target.setText(target);
+                        userdata.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(AVException e) {
+                                if (e == null){
+                                    t_height.setText(height);
+                                    t_weight.setText(weight);
+                                    t_target.setText(target);
+                                    Toast.makeText(getActivity(), "更新成功~", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
                     }
                 });
                 break;
@@ -88,6 +99,7 @@ public class ThirdFragment extends Fragment implements View.OnClickListener {
         t_height = (EditText) view.findViewById(R.id.t_height);
         t_weight = (EditText) view.findViewById(R.id.t_weight);
         t_target = (EditText) view.findViewById(R.id.t_target);
+        sPref = getActivity().getSharedPreferences("userdataId", MODE_PRIVATE);
     }
 
     void initData() {
@@ -100,9 +112,9 @@ public class ThirdFragment extends Fragment implements View.OnClickListener {
             t_email.setText(email);
         }
         //根据用户查到关联的userdata
-        AVQuery<AVObject> userdata = new AVQuery<>("UserData");
-        userdata.include("username");
-        userdata.findInBackground(new FindCallback<AVObject>() {
+        AVQuery<AVObject> userData = new AVQuery<>("UserData");
+        userData.include("username");
+        userData.findInBackground(new FindCallback<AVObject>() {
             @Override
             public void done(List<AVObject> list, AVException e) {
                 if (e == null && list.size() > 0) {
@@ -114,13 +126,20 @@ public class ThirdFragment extends Fragment implements View.OnClickListener {
                     t_weight.setText(weight);
                     t_target.setText(target);
                 } else {
-                    AVObject userdata = new AVObject("UserData");
-                    userdata.put("username", AVUser.getCurrentUser());
+                    final AVObject userdata = new AVObject("UserData");
+                    userdata.put("user", AVUser.getCurrentUser());
                     userdata.put("height", "170");
                     userdata.put("weight", "60");
                     userdata.put("target", "3");
-                    userdata.saveInBackground();
-                    userdataId = userdata.getObjectId();
+                    userdata.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(AVException e) {
+                            if (e == null) {
+                                // 存储成功,把userdataId保存下来
+                                sPref.edit().putString("userdataId", userdata.getObjectId()).commit();
+                            }
+                        }
+                    });
                     t_height.setText("170");
                     t_weight.setText("60");
                     t_target.setText("3");
